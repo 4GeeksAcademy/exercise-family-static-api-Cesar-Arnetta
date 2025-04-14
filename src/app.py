08e2628106d4a84e349a450897768386167d7795
cesararnetta <cesararnetta@gmail.com>
2025-04-14 19:33:19 +0000

    Exercise done

--- src/app.py
+++ src/app.py
@@ -29,17 +29,65 @@ def sitemap():
     return generate_sitemap(app)
 
 
+# GET all the members of the family
+
 @app.route('/members', methods=['GET'])
-def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
+def get_all_members():
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
+
+# GET a member by ID, remember we use <int:member_id> to indicate the end of the route witch contains the id as a parameter.
+# Also the function get_one_member call the method get_member
+
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_one_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member is None:
+        return jsonify({"error": "Member not found"}), 404
+    print(member)
+
+    return jsonify({
+        "id": member["id"],
+        "first_name": member["first_name"],
+        "age": member["age"],
+        "lucky_numbers": member["lucky_numbers"]
+    }), 200
 
 
+# POST / add a member.
 
-# This only runs if `$ python src/app.py` is executed
+@app.route('/members', methods=['POST'])
+def add_new_member():
+    # Similar to a promise declaration we use the method try in POST method
+    try:
+        data = request.get_json()
+        if not data:
+            return jsonify({"error": "Missing JSON body"}), 400
+        
+        # Basic validation last name always by a default value, but in other cases name. age and lucky are "inputs" in the API
+        if "first_name" not in data or "age" not in data or "lucky_numbers" not in data:
+            return jsonify({"error": "Missing required fields"}), 400
+        
+        # We call the method of the datastructure in this line and return the resolve promise jsonify. Important for the post method
+        add_new_member = jackson_family.add_member(data)
+        return jsonify(add_new_member), 200
+    
+# This allow us to "catch" the error during the request 
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# DELETE method.
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        jackson_family.delete_member(member_id)
+        return jsonify({"done": True}), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
+
+# This only runs if `$ python src/app.py` is executed. For this reason when we write in the terminar this command the server turn on
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/datastructures.py
+++ src/datastructures.py
@@ -8,7 +8,7 @@ Update this file to implement the following already declared methods:
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-        self._next_id = 1
+        self._next_id = 0
         self._members = [
             {
                 "id": self._generate_id(),
@@ -16,6 +16,20 @@ class FamilyStructure:
                 "last_name": last_name,
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
             }
         ]
 
@@ -25,21 +39,73 @@ class FamilyStructure:
         self._next_id += 1
         return generated_id
 
+# First we check if the value member's method have an id inside of init list of self.members. If not, we use the _generate_id method
+# to add it. Last we check if the member have the last name, if not we use de init value of last name, becauase all are part of the same family.
+# Finally we use the python's method append to add this member to the family
+# The final return is important to close the method 
+
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
-        pass
+        if "id" not in member:
+            member["id"] = self._generate_id()
+        if "last_name" not in member:
+            member["last_name"] = self.last_name
+        self._members.append(member)
+        return member
+
+# Using a loop for member in  self._members we create a conditional if members["id"] is the same of the id's parameter, we finally use
+# the remove python's method to members
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+       for member in self._members:
+            if member["id"] == id:
+                self._members.remove(member)
+                return
+            
+# Using a loop for member in  self._members we create a conditional if members["id"] is the same of the id's parameter, we finally use
+# return the chose member if there is no existed member, the metjod return None 
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                return member
+        return None 
 
-    # This method is done, it returns a list with all the family members
+    # This method is done, it returns a list with all the family members / it's the default method of the exercise
     def get_all_members(self):
         return self._members
+    
+# These are the testing lines 
+
+# John Jackson
+# 33 Years old
+# Lucky Numbers: 7, 13, 22
+
+# Jane Jackson
+# 35 Years old
+# Lucky Numbers: 10, 14, 3
+
+# Jimmy Jackson
+# 5 Years old
+# Lucky Numbers: 1
+
+# family = FamilyStructure("Jackson")
+
+# family.add_member({
+#     "first_name": "Jane",
+#     "age": 35,
+#     "lucky_numbers": [10, 14, 3]
+# })
+
+# # Get a member by ID
+# member = family.get_member(1)  # Should return Jhon Jackson
+# print(member)
+
+# members = family.get_all_members()
+# print(members)
+
+# # Delete a member by ID
+# family.delete_member(1)  # Removes Jhon Jackson who is declared in the __init__ 
+
+# # Get all members after deletion withou Jhon
+# members = family.get_all_members()
+# print(members)
